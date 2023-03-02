c79185b53a983da3859d333794e892311f361c8b
Binyam Tafesse <tafessebinyam51@gmail.com>
2023-03-02 16:18:17 -0600

    first commit

---
+++ mysql.java/src/main/java/projects/exceptions/DdbExceptions.java
@@ -0,0 +1,28 @@
+package projects.exceptions;
+
+
+@SuppressWarnings("serial")
+public class DdbExceptions extends RuntimeException {
+
+	public DdbExceptions() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public DdbExceptions(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DdbExceptions(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DdbExceptions(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+
+}
